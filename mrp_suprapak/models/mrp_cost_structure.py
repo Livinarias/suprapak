7e50278647f064d856fdf5bdc17dcf17c13db1a3
Oscar B <ob@todoo.co>
2020-04-15 22:23:25 -0500

    add cost_planned lines

--- mrp_suprapak/models/mrp_cost_structure.py
+++ mrp_suprapak/models/mrp_cost_structure.py
@@ -30,11 +30,11 @@ class MrpCostStructure(models.AbstractModel):
                 self.env.cr.execute(query_str, (tuple(Workorders.ids), ))
                 for op_id, op_name, user, duration, cost_hour, cost_hour_mod, costs_hour_cif, costs_hour_maq in self.env.cr.fetchall():
                     operations_line.append([user, op_id, op_name, duration / 60.0, cost_hour, cost_hour_mod, costs_hour_cif, costs_hour_maq])
-            res[count]['operations_line'] = operations_line 
+            res[count]['operations_line'] = operations_line
             #get the cost of raw material effectively used
             raw_material_moves_line = []
             query_str = """SELECT sm.product_id, sm.bom_line_id, abs(SUM(sml.qty_done)), abs(SUM(sm.price_unit))
-                             FROM stock_move AS sm 
+                             FROM stock_move AS sm
                              INNER JOIN stock_move_line AS sml ON sml.move_id = sm.id
                             WHERE sm.raw_material_production_id in %s AND sm.state != 'cancel' AND sm.product_qty != 0 AND scrapped != 't'
                          GROUP BY sm.bom_line_id, sm.product_id"""
@@ -46,6 +46,31 @@ class MrpCostStructure(models.AbstractModel):
                     'product_id': ProductProduct.browse(product_id),
                     'bom_line_id': bom_line_id
                 })
-            res[count]['raw_material_moves_line'] = raw_material_moves_line 
+            res[count]['raw_material_moves_line'] = raw_material_moves_line
+            # Costs before
+            cost_planned = []
+            mrp_productions = self.env['mrp.production'].browse(mos.ids)
+            for mrp in mrp_productions:
+                for i, line in enumerate(mrp.finished_move_line_ids):
+                    bom_total = 0
+                    cost_planned.append({
+                        'product': line.product_id,
+                        'initial_qty': line.product_uom_qty,
+                        'cost': line.product_id.standard_price,
+                        'components': None,
+                        'total': bom_total,
+                    })
+                    components = []
+                    for bom_line in mrp.bom_id.bom_line_ids:
+                        components.append({
+                            'product': bom_line.product_id,
+                            'qty': bom_line.product_qty * cost_planned[i]['initial_qty'],
+                            'cost': bom_line.product_id.standard_price * cost_planned[i]['initial_qty'],
+                            'BoM_cost': bom_line.product_id.standard_price,
+                        })
+                        bom_total += bom_line.product_id.standard_price
+                    cost_planned[i]['components'] = components
+                    cost_planned[i]['total'] += bom_total
+            res[count]['cost_planned'] = cost_planned
             count += 1
         return res
