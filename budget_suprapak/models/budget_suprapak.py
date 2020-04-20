041a0039137039a4466ad20d65451950026fc923
LIvingston <la@todoo.co>
2020-04-20 08:27:02 -0500

    17934[ADD]cambios en desarrollo de prosupuesto

--- budget_suprapak/models/budget_suprapak.py
+++ budget_suprapak/models/budget_suprapak.py
@@ -3,11 +3,9 @@ from odoo import models,api,fields
 class crossoveredBudget(models.Model):
     _inherit = 'purchase.order'
 
-    #responsable = fields.Many2one()
-
     def validation_budget(self):
         if self.practical_amount > self.planned_amount:
             wiz_id = self.env['notification.budget']
         else:
-            self.button_confirm()
+            self.action_view_invoice()
 
--- budget_suprapak/wizard/budget_wizard.py
+++ budget_suprapak/wizard/budget_wizard.py
@@ -5,8 +5,26 @@ class BudgetTemplate(models.TransientModel):
     _description = 'This is a wizard to limit Budget'
 
     def button_send_mail(self):
-        self.send_notification()
+        self.action_create_activity()
+        #self.send_notification()
         return True
+
+    def action_create_activity(self):
+        order_id = self.env['pruchase.order'].browse(self._context.get('active_id'))
+        model_id = self.env.ref('purchase.model_purchase_order')
+        type_id = self.env.ref('mail.mail_activity_data_todo')
+        summary = 'El pedido ha sido bloqueado por superar el presupuesto, por favor revisar'
+        date_deadline = order_id.validity_date if order_id.validity_date else fields.Date.today()
+        for user in order_id.users_ids:
+            activity_data = {
+                'res_id': order_id.id,
+                'res_model_id': model_id.id,
+                'activity_type_id': type_id.id,
+                'date_deadline': date_deadline,
+                'summary': summary,
+                'user_id': user.id,
+            }
+            self.env['mail.activity'].create(activity_data)
 
     def send_notification(self):
         template_id = self.env.ref('budget_suprapak.mail_template_budget_suprapak').id
